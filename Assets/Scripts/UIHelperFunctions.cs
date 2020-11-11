dceb31d2dc9137412bde7fe49765c9d9c777b88d
Jeremy Epp <jeremy.epp@outlook.com>
2020-11-11 16:19:12 -0600

    Add main menu and options scenes for UI

---
+++ Assets/Scripts/UIHelperFunctions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class UIHelperFunctions : MonoBehaviour
+{
+    public void LoadScene(string scene_name)
+    {
+        SceneManager.LoadScene(scene_name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
