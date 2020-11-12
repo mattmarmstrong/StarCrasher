b273f49ffe6cfa128ca82d0c75b55ad46239b3f6
Jeremy Epp <jeremy.epp@outlook.com>
2020-11-12 14:08:51 -0600

    Fix bug where game started paused.

--- Assets/Scripts/UIHelperFunctions.cs
+++ Assets/Scripts/UIHelperFunctions.cs
@@ -7,6 +7,9 @@ public class UIHelperFunctions : MonoBehaviour
 {
     public void LoadScene(string scene_name)
     {
+        // Restart game timer when game scene is loaded
+        if (scene_name == "Main Scene")
+            Time.timeScale = 1;
         SceneManager.LoadScene(scene_name);
     }
 
